4dcb4f2eb04861b58258da91c3b83ed6c2e95865
nvp85 <nvp@zhukov.al>
2025-03-16 21:45:13 -0500

    add two more scenarios

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,16 +28,47 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+let coins = 12;
+let hasSword = false;
+let hasCompass = false;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
 if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
+
+  console.log("You see a figure on the road. It's a bandit!");
+  const choice = readline.question("Do you 'fight' the bandit or 'hide' before he notices you?");
+
+  if (choice === "fight" && hasSword) {
+    console.log("You fight off the bandit and continue your journey.");
+  } else if (choice === "fight" && !hasSword) {
+    console.log("You lose the fight. The bandit steals your coins.");
+    coins = 0;
+  } else {
+    console.log("You hide in bushes and the bandit doesn't see you.");
+  }
+
 } else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
 } else if (choice === "village" || hasMap) {
   console.log("You find your way to the village.");
+
+  console.log("In the village you meet a merchant who sells swords(10 coins) and a compass(10 coins).");
+  const choice = readline.question("Do you want to buy a 'sword', 'compass' or 'nothing'?");
+  if ((choice === 'sword' || choice === 'compass') && coins < 10) {
+    console.log("You don't have enough coins.");
+  } else if (choice === 'sword') {
+    console.log("You buy a sword");
+    hasSword = true;
+    coins -= 10;
+  } else if (choice === 'compass') {
+    console.log("You buy a compass");
+    hasCompass = true;
+    coins -= 10;
+  } 
+
 } else {
   console.log("You get lost and wander aimlessly.");
 }
